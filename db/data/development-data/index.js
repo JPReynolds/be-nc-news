baadb7d131eaa59402f8f8969cc72a7f906eb4e2
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-03-23 11:59:43 +0000

    set up project, exporting data into index files

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const articleData = require('./articles');
+const topicData = require('./topics');
+const userData = require('./users');
+const commentData = require('./comments');
+
+module.exports = { articleData, topicData, userData, commentData };
