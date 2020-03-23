c8cf1f31b12a81565f331642f2a6fe0734432405
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-03-23 13:46:40 +0000

    created formatDate function and tested

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,40 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  if (list.length === 0) return [];
+
+  const formattedDates = list.map(object => {
+    const date = new Date(object.created_at);
+
+    const year = date.getFullYear();
+
+    const month = date.getMonth();
+
+    const day = date.getDate();
+
+    const hours = date.getHours();
+
+    const minutes = date.getMinutes();
+
+    const seconds = date.getSeconds();
+
+    const newDate =
+      month +
+      '-' +
+      day +
+      '-' +
+      year +
+      ' ' +
+      hours +
+      ':' +
+      minutes +
+      ':' +
+      seconds;
+
+    const newObj = { ...object };
+    newObj.created_at = newDate;
+    return newObj;
+  });
+  return formattedDates;
+};
 
 exports.makeRefObj = list => {};
 
