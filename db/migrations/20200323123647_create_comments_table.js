05d14bbb3cfc0076d055dffabd66006b3e40ae90
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-03-23 12:43:15 +0000

    added comments migration

---
+++ db/migrations/20200323123647_create_comments_table.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  console.log('creating commments table');
+  return knex.schema.createTable('comments', commentsTable => {
+    commentsTable.increments('comment_id');
+    commentsTable
+      .string('author')
+      .references('users.username')
+      .notNullable();
+    commentsTable
+      .integer('article_id')
+      .references('articles.article_id')
+      .notNullable();
+    commentsTable.bigInteger('votes').defaultTo(0);
+    commentsTable.timestamp('created_at');
+    commentsTable.string('body');
+  });
+};
+
+exports.down = function(knex) {
+  console.log('dropping comments table');
+  return knex.schema.dropTable('comments');
+};
