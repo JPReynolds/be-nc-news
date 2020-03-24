7ddd6a9ab075b07f6dc74c88543ab6ae4458b490
JPReynolds <JordanReynolds@jordans-mbp-3.home>
2020-03-24 11:24:02 +0000

    created error folder and added 500 error handler

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-
 app.use(express.json());
-
 const apiRouter = require('./routes/apiRouter');
+const { handle500s } = require('./errors');
 
 app.use('/api', apiRouter);
 
 app.all('/*', (req, res, next) => {
   res.status(404).send({ msg: 'path not found' });
 });
+
+app.use(handle500s);
 
 module.exports = app;
